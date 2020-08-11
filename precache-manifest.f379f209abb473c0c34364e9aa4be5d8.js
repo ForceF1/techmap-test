83c0315697f4e4dd66240a8bda367b0f9c67abf6
vitalii.karetin <forcef12@gmail.com>
2020-08-11 15:27:21 +0300

    Updates

---
+++ precache-manifest.f379f209abb473c0c34364e9aa4be5d8.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4936e132de6d27814775e3cc6ea638be",
+    "url": "/techmap-test/index.html"
+  },
+  {
+    "revision": "b4b5b004d890dc00e06e",
+    "url": "/techmap-test/static/css/main.1a6d3aa0.chunk.css"
+  },
+  {
+    "revision": "f5743295d74fdae1cc1c",
+    "url": "/techmap-test/static/js/14.023a0544.chunk.js"
+  },
+  {
+    "revision": "9c610ef51f8ae02205b4ac8c3073fb22",
+    "url": "/techmap-test/static/js/14.023a0544.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4b5b004d890dc00e06e",
+    "url": "/techmap-test/static/js/main.c658abb3.chunk.js"
+  },
+  {
+    "revision": "e623d53f8ab3855c488e",
+    "url": "/techmap-test/static/js/reactPlayerDailyMotion.62afb489.chunk.js"
+  },
+  {
+    "revision": "b8db29cc26dc9fcb2c99",
+    "url": "/techmap-test/static/js/reactPlayerFacebook.c6cdc431.chunk.js"
+  },
+  {
+    "revision": "48a4f36655d0906de2dc",
+    "url": "/techmap-test/static/js/reactPlayerFilePlayer.d5784aed.chunk.js"
+  },
+  {
+    "revision": "6c98152c7ad66a71d883",
+    "url": "/techmap-test/static/js/reactPlayerMixcloud.b4d8a8ef.chunk.js"
+  },
+  {
+    "revision": "af917ce6e4eda0a9b37c",
+    "url": "/techmap-test/static/js/reactPlayerPreview.de08d8bc.chunk.js"
+  },
+  {
+    "revision": "a62bca1c1a46e6275d14",
+    "url": "/techmap-test/static/js/reactPlayerSoundCloud.fa85aff6.chunk.js"
+  },
+  {
+    "revision": "01354e03104720e8ac16",
+    "url": "/techmap-test/static/js/reactPlayerStreamable.c56da7f1.chunk.js"
+  },
+  {
+    "revision": "8fd132acd0c7a8e58619",
+    "url": "/techmap-test/static/js/reactPlayerTwitch.9923b69e.chunk.js"
+  },
+  {
+    "revision": "0d0316778f174e4ae556",
+    "url": "/techmap-test/static/js/reactPlayerVidyard.516a8381.chunk.js"
+  },
+  {
+    "revision": "d2139ea4cba33234ce4e",
+    "url": "/techmap-test/static/js/reactPlayerVimeo.d2199687.chunk.js"
+  },
+  {
+    "revision": "5251e6888f7ac6e0f6c7",
+    "url": "/techmap-test/static/js/reactPlayerWistia.464cc997.chunk.js"
+  },
+  {
+    "revision": "c405c67583c3705ac69a",
+    "url": "/techmap-test/static/js/reactPlayerYouTube.9e6359d0.chunk.js"
+  },
+  {
+    "revision": "b5963422551679246c95",
+    "url": "/techmap-test/static/js/runtime-main.133346ca.js"
+  },
+  {
+    "revision": "1ed39878bc9fb6684e780d6bae5d0992",
+    "url": "/techmap-test/static/media/BIAndDataTechIcon.1ed39878.svg"
+  },
+  {
+    "revision": "e57e6c0675d3df885ff7b3f84bc9c1dc",
+    "url": "/techmap-test/static/media/PlayButton.e57e6c06.svg"
+  },
+  {
+    "revision": "91543de0ad44b64b468d1e93f12b8a66",
+    "url": "/techmap-test/static/media/about-img1.91543de0.png"
+  },
+  {
+    "revision": "ba708b91b6bc5a3bfc49efd3e76d8c52",
+    "url": "/techmap-test/static/media/about-img2.ba708b91.png"
+  },
+  {
+    "revision": "d786c6336745f62ec1de103d1c2e1a82",
+    "url": "/techmap-test/static/media/about-img3.d786c633.png"
+  },
+  {
+    "revision": "d3a5d65f0e66b6b33e87a0591abac9af",
+    "url": "/techmap-test/static/media/about-img4.d3a5d65f.png"
+  },
+  {
+    "revision": "f684077bdb10e5f214460170fb978890",
+    "url": "/techmap-test/static/media/adTechIcon.f684077b.svg"
+  },
+  {
+    "revision": "f50dd5b02c2f65cccd859a321bef3380",
+    "url": "/techmap-test/static/media/contact_photo_1.f50dd5b0.png"
+  },
+  {
+    "revision": "8d88eb5aadd0da661aad56119e30bf92",
+    "url": "/techmap-test/static/media/contact_photo_2.8d88eb5a.png"
+  },
+  {
+    "revision": "8468a7dae548893f8f623f6833ec7455",
+    "url": "/techmap-test/static/media/contact_photo_3.8468a7da.png"
+  },
+  {
+    "revision": "aacffd3da650f8989574552f36eb9fe4",
+    "url": "/techmap-test/static/media/customerTechIcon.aacffd3d.svg"
+  },
+  {
+    "revision": "a2fce0fe45f53991ccbc6356556202dc",
+    "url": "/techmap-test/static/media/devTechIcon.a2fce0fe.svg"
+  },
+  {
+    "revision": "7cc9ef782bdfb85c4b6ad09f139b1b13",
+    "url": "/techmap-test/static/media/ecommerceIcon.7cc9ef78.svg"
+  },
+  {
+    "revision": "e1d68a4ce987d1ec5915a67f0e7b94a9",
+    "url": "/techmap-test/static/media/hrTechIcon.e1d68a4c.svg"
+  },
+  {
+    "revision": "904658cf7f6aa7f5c24624bca24aed09",
+    "url": "/techmap-test/static/media/logo.904658cf.svg"
+  },
+  {
+    "revision": "6cef80059395bc79fc87f2086b2c4d61",
+    "url": "/techmap-test/static/media/marTechIcon.6cef8005.svg"
+  },
+  {
+    "revision": "072739753532d418084993bbfdf4592a",
+    "url": "/techmap-test/static/media/salesTechIcon.07273975.svg"
+  },
+  {
+    "revision": "2d831251e4b18d4e692bc89bd50a9cfd",
+    "url": "/techmap-test/static/media/support-img1.2d831251.png"
+  },
+  {
+    "revision": "2516d572f149cfd362c9ad1e1641b8f6",
+    "url": "/techmap-test/static/media/support-img2.2516d572.png"
+  },
+  {
+    "revision": "655fd974d2b9bec87f60cd550b4e19fb",
+    "url": "/techmap-test/static/media/support-img3.655fd974.png"
+  },
+  {
+    "revision": "e1b79f2549e50114916c3c62e7c56edb",
+    "url": "/techmap-test/static/media/workplaceTechIcon.e1b79f25.svg"
+  }
+]);
